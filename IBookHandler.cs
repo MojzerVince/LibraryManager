3c855c08e45e2bb22f3b4ebe0649771d08d1b2b2
Mojzi Lili_PC <mojzer.vince@blathy.info>
2024-05-06 14:50:20 +0200

    another solution for loading books

--- IBookHandler.cs
+++ IBookHandler.cs
@@ -6,39 +6,29 @@ using System.Threading.Tasks;
 
 namespace LibraryManager
 {
-    struct Books
-    {
-        public int book_id;
-        public string title;
-        public string author;
-        public string rl_date; //release date
-        public int price;
-        public byte quant_avail; //quantity available
-    }
-
     internal class IBookHandler
     {
-        static int n = 0;
-        static Books[] books = new Books[100];
 
         public void BookLoad()
         {
             StreamReader sr = new StreamReader("books.txt");
+            sr.ReadLine(); //kihagyja az 1. sort, amiben csak a format van
             while (!sr.EndOfStream)
             {
-                string[] data = Console.ReadLine().Split("|");
-                books[n].book_id = int.Parse(data[0]);
-                books[n].title = data[1];
-                books[n].author = data[2];
-                books[n].rl_date = data[3];
-                books[n].price = int.Parse(data[4]);
-                books[n].quant_avail = byte.Parse(data[5]);
-
-                n++;
+                string[] data = sr.ReadLine().Split("|");
+                for (int i = 0; i < data.Length; i++)
+                {
+                    Console.Write($"{data[i++]}|{data[i++]}|{data[i++]}|{data[i++]}|{data[i++]}|{data[i++]}\n");
+                }
             }
         }
 
-        public void AddEntry()
+        protected void AddEntry()
+        {
+
+        }
+
+        protected void RemoveEntry()
         {
 
         }
