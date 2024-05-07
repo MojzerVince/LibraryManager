171c185ae300b154cd8a3b493e0cbec6095f0b03
Mojzi Lili_PC <mojzer.vince@blathy.info>
2024-05-07 13:04:29 +0200

    fixed id generation for students

--- Program.cs
+++ Program.cs
@@ -34,6 +34,10 @@ namespace LibraryManager
                     else if (opt == "2") StudentLogin();
                     else Console.WriteLine("An unexpected error occured!"); //leszarom a kivételkezelést
                     break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine($"Please choose from the options! You pressed: {key.Key}");
+                    break;
             }
         }
 
@@ -44,7 +48,7 @@ namespace LibraryManager
             while(!sr.EndOfStream)
             {
                 string line = sr.ReadLine();
-                string[] data = line.Split(" ");
+                string[] data = line.Split("|");
 
                 pass[n].username = data[0];
                 pass[n].password = data[1];
@@ -52,7 +56,10 @@ namespace LibraryManager
                 if (pass[n].username == username && pass[n].password == password)
                 {
                     Console.WriteLine("Success " + type);
-                    if(type == "admin")
+                    Thread.Sleep(500);
+                    Console.WriteLine("Loading...");
+                    Thread.Sleep(1500);
+                    if (type == "admin")
                     {
                         Admin admin = new Admin(username, password);
                         admin.DashBoard();
@@ -103,37 +110,37 @@ namespace LibraryManager
 
             Console.Write("Student name: ");
             string name = Console.ReadLine();
-            sw.Write("\n" + name + " ");
+            sw.Write("\n" + name + "|");
             Console.Write("Student card number: ");
-            int number = int.Parse(Console.ReadLine());
+            string number = Console.ReadLine();
             Console.Write("Student email: ");
             string email = Console.ReadLine();
             Console.Write("Account password: ");
             string password = Console.ReadLine();
-            sw.Write(password  + " ");
+            sw.Write(password);
             Console.Write("Student birth date (yyyy-mm-dd): ");
             string bdate = Console.ReadLine();
             Console.Write("Student contact: ");
             string contact = Console.ReadLine();
             sw.Close();
 
+            //-- automata id hozzárendelés
             StreamReader sr = new StreamReader("students.txt");
-            //string[] data;
-            string line;
-            if (sr.EndOfStream)
-            {
-                //data = sr.ReadLine().Split("|");
-                line = sr.ReadLine();
-            }
-            else line = "";
-
-            //int id = int.Parse(data[0]);
+            string line = "";
+            //ha van már sor, akkor megnézi a legutolsót és annak az id-jához ad plusz 1-et
+            while (!sr.EndOfStream) line = sr.ReadLine();
+            //else line = ""; //ha nincs, akkor legyen az első sor üres, ez később if-ben hasznos
+            Console.WriteLine("sor: " + line);
+            sr.Close();
             int id;
-            if (line == "") id = int.Parse(line.Substring(0, line.IndexOf("|")))+1;
-            else id = 0;
+            //ha már van sor, akkor kiszedő az utolsóból az id-t és hozzáad 1-et
+            if (line != "") id = int.Parse(line.Substring(0, line.IndexOf("|")))+1;
+            else id = 0; //ha nincs még sor, akkor automatán 0 lesz az id
+
+            //Student példányosítás
             Student std = new Student(id, name, email, number, password, bdate, contact);
-            students.Add(std);
-            //StudentLogin();
+            students.Add(std); //példány hozzáadás a tanulókat tartalmazó listába
+            StudentLogin(); //regisztráció utáni bejelentkeztetés
         }
     }
 }
