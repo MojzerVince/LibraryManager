65e46d419fda1619fd7d610ca4784230942e00d2
Mojzi Lili_PC <mojzer.vince@blathy.info>
2024-05-07 10:33:39 +0200

    Student account registration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LibraryManager
+﻿using System.Runtime.CompilerServices;
+
+namespace LibraryManager
 {
     struct Pass
     {
@@ -10,10 +12,10 @@
     {
         static int n = 0;
         static Pass[] pass = new Pass[10];
+        static List<Student> students = new List<Student>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! \n");
             Console.Write("Admin(1)/Student(2) Login: ");
 
             ConsoleKeyInfo key = Console.ReadKey(true);
@@ -25,8 +27,12 @@
                     break;
                 case ConsoleKey.NumPad2:
                 case ConsoleKey.D2:
-                    //Student student = new Student();
-                    StudentLogin();
+                    Console.Clear();
+                    Console.Write("Student profile registration(1) | Student login(2) ");
+                    string opt = Console.ReadLine();
+                    if (opt == "1") StudentReg();
+                    else if (opt == "2") StudentLogin();
+                    else Console.WriteLine("An unexpected error occured!"); //leszarom a kivételkezelést
                     break;
             }
         }
@@ -46,10 +52,20 @@
                 if (pass[n].username == username && pass[n].password == password)
                 {
                     Console.WriteLine("Success " + type);
+                    if(type == "admin")
+                    {
+                        Admin admin = new Admin(username, password);
+                        admin.DashBoard();
+                    }
+                    else if(type == "student")
+                    {
+                        //idk
+                    }
                     break; //ha megtalálta, akkorne fusson tovább feleslegesen
                 }
                 n++;
             }
+            sr.Close();
         }
 
         static void AdminLogin()
@@ -63,9 +79,6 @@
             string password = Console.ReadLine();
 
             Load(username, password, "admin");
-
-            Admin admin = new Admin(username, password);
-            admin.DashBoard();
         }
 
         static void StudentLogin()
@@ -79,6 +92,48 @@
             string password = Console.ReadLine();
 
             Load(username, password, "student");
+        }
+
+        static void StudentReg()
+        {
+            Console.Clear();
+            Console.WriteLine("STUDENT REGISTRATION");
+
+            StreamWriter sw = new StreamWriter("studentlogins.txt", true); //append
+
+            Console.Write("Student name: ");
+            string name = Console.ReadLine();
+            sw.Write("\n" + name + " ");
+            Console.Write("Student card number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Student email: ");
+            string email = Console.ReadLine();
+            Console.Write("Account password: ");
+            string password = Console.ReadLine();
+            sw.Write(password  + " ");
+            Console.Write("Student birth date (yyyy-mm-dd): ");
+            string bdate = Console.ReadLine();
+            Console.Write("Student contact: ");
+            string contact = Console.ReadLine();
+            sw.Close();
+
+            StreamReader sr = new StreamReader("students.txt");
+            //string[] data;
+            string line;
+            if (sr.EndOfStream)
+            {
+                //data = sr.ReadLine().Split("|");
+                line = sr.ReadLine();
+            }
+            else line = "";
+
+            //int id = int.Parse(data[0]);
+            int id;
+            if (line == "") id = int.Parse(line.Substring(0, line.IndexOf("|")))+1;
+            else id = 0;
+            Student std = new Student(id, name, email, number, password, bdate, contact);
+            students.Add(std);
+            //StudentLogin();
         }
     }
 }
