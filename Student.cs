e5383fc4955abab7f3a1adb25c6d71ff34acf57b
Mojzi Lili_PC <mojzer.vince@blathy.info>
2024-05-07 13:05:04 +0200

    card number changed to string due to length problems with ints

--- Student.cs
+++ Student.cs
@@ -11,7 +11,7 @@ namespace LibraryManager
         private int std_id;
         private string std_name;
         private string std_email;
-        private int std_no; //kártyaszám
+        private string std_no; //kártyaszám
         private string password;
         private string std_bdate;
         private string contact; //telefonszám
@@ -19,12 +19,12 @@ namespace LibraryManager
         public int Std_id { get => std_id; set => std_id = value; }
         public string Std_name { get => std_name; set => std_name = value; }
         public string Std_email { get => std_email; set => std_email = value; }
-        public int Std_no { get => std_no; set => std_no = value; }
+        public string Std_no { get => std_no; set => std_no = value; }
         public string Password { get => password; private set => password = value; }
         public string Std_bdate { get => std_bdate; set => std_bdate = value; }
         public string Contact { get => contact; set => contact = value; }
 
-        public Student(int std_id, string std_name, string std_email, int std_no, string password, string std_bdate, string contact)
+        public Student(int std_id, string std_name, string std_email, string std_no, string password, string std_bdate, string contact)
         {
             Std_id = std_id;
             Std_name = std_name;
@@ -35,7 +35,7 @@ namespace LibraryManager
             Contact = contact;
 
             StreamWriter sw = new StreamWriter("students.txt", true);
-            sw.WriteLine($"{std_id}|{std_name}|{std_email}|{std_no}|{password}|{std_bdate}|{contact}");
+            sw.WriteLine($"{std_id}|{std_name}|{std_email}|{std_no}|{password}|{std_bdate}|{contact}\n");
             sw.Close();
         }
     }
