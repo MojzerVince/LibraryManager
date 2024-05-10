9fb8137757dcf9eb592ab4e08741606d2000ac71
Mojzi Lili_PC <mojzer.vince@blathy.info>
2024-05-10 09:13:45 +0200

    admin navbar

--- HTML/main.js
+++ HTML/main.js
@@ -3,17 +3,28 @@ let loginClass = document.querySelector(".login");
 let admin = document.querySelector(".ad_logged");
 let student = document.querySelector(".std_logged");
 
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-    loginClass.style = "display: none;";
-    console.log("LOGGED IN");
+form.addEventListener("submit", function (event) {
+
 
     let formData = new FormData(form);
-    if(formData.get("type") == 'admin'){
-        admin.style = "display: grid;";
+    if (formData.get("password") != "") {
+        event.preventDefault();
+        loginClass.style = "display: none;";
+        console.log("LOGGED IN");
+
+        if (formData.get("type") == 'admin') {
+            admin.style = "display: grid;";
+
+            let ad_user = document.querySelector("#ad_user");
+            ad_user.innerHTML = formData.get("username");
+        }
+        else {
+            student.style = "display: grid;";
+        }
     }
-    else{
-        student.style = "display: grid;";
-    }
-    //console.log(formData.getAll("type"));
+    else alert("Please enter password!");
 })
+
+function Books(){
+    
+}
